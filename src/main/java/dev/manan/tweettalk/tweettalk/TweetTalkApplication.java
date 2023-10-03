d8f5f0dd3d0307af4fb042cd74c184c7b6ba05fc
Manan Solanki <mananrsolanki@gmail.com>
2023-10-03 23:28:27 +0530

    Project init

---
+++ src/main/java/dev/manan/tweettalk/tweettalk/TweetTalkApplication.java
@@ -0,0 +1,13 @@
+package dev.manan.tweettalk.tweettalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/manan/tweettalk/tweettalk/TweetTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manan.tweettalk.tweettalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
